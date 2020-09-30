24d9b04d3974e656ff1a1acd85cc0ac2ac57f3bb
Josh Belmont <jbelmont19@live.com>
2020-09-30 09:12:44 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Josh Belmont on 9/30/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
